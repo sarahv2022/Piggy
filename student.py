f3876347c8bcae5e07af15e6b6df8df4fee93947
Sarah voss <91563298+sarahv2022@users.noreply.github.com>
2021-11-09 19:18:39 +0000

    fun stuff :)

--- student.py
+++ student.py
@@ -69,6 +69,7 @@ class Piggy(PiggyParent):
         # TODO: check to see if it's safe before dancing
         
         # lower-ordered example...
+        self.shake()
         self.right(primary=50, counter=50)
         time.sleep(2)
         self.stop()
