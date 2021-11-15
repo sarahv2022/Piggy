2dc0722ab34faae4d79ccb9ca26548ead5d20582
Sarah voss <91563298+sarahv2022@users.noreply.github.com>
2021-11-15 20:14:16 +0000

    not all stop

--- student.py
+++ student.py
@@ -63,12 +63,10 @@ class Piggy(PiggyParent):
         if self.read_distance() < 150:
           self.stop()
           self.turn_by_deg(90)
-          self.stop()
           self.fwd()
           time.sleep(1)
           self.stop()
           self.turn_by_deg(-90)
-          self.stop()
           break
         else:
           self.fwd()
